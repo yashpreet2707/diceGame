b7c008dbc7c48138fd14baef4d7fc8fdffd19ce6
yashpreet2707 <yashpreet1686@gmail.com>
2023-05-11 21:54:35 +0530

    fixed img issue

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ var randomNumber1 = Math.floor(Math.random() * 6 ) + 1 ;
 var randomDiceImage1 = randomNumber1 + ".png" ;
 var randomImageSource1 = "./images/" + randomDiceImage1 ;
 
-var image1 = document.querySelectorAll("img")[0];
+var image1 = document.querySelectorAll("img")[1];
 image1.setAttribute("src", randomImageSource1 ) ;
 
 var randomNumber2 = Math.floor(Math.random() * 6 ) + 1 ;
 var randomDiceImage2 = randomNumber2 + ".png" ;
 var randomImageSource2 = "./images/" + randomDiceImage2 ;
 
-var image2 = document.querySelectorAll("img")[1];
+var image2 = document.querySelectorAll("img")[2];
 image2.setAttribute("src", randomImageSource2 ) ;
 
 
