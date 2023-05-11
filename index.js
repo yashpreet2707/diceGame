cc68941f4eb713193bfc4d3d9269730ef38323a4
yashpreet2707 <yashpreet1686@gmail.com>
2023-05-11 21:43:27 +0530

    added name functionality

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+
+var player1 = prompt("What is Player 1 name?");
+var player2 = prompt("What is Player 2 name?");
+
+document.querySelectorAll("p")[0].innerHTML = player1 ;
+document.querySelectorAll("p")[1].innerHTML = player2 ;
+
+
 var randomNumber1 = Math.floor(Math.random() * 6 ) + 1 ;
 var randomDiceImage1 = randomNumber1 + ".png" ;
 var randomImageSource1 = "./images/" + randomDiceImage1 ;
@@ -14,10 +22,10 @@ image2.setAttribute("src", randomImageSource2 ) ;
 
 
 if ( randomNumber1 > randomNumber2 ) {
-    document.querySelector("h1").innerHTML = "Player 1 is the winner 🥳" ;
+    document.querySelector("h1").innerHTML = player1 + " is the winner 🥳" ;
 }
 else if ( randomNumber1 < randomNumber2 ) {
-    document.querySelector("h1").innerHTML = "Player 2 is the winner 🥳" ;
+    document.querySelector("h1").innerHTML = player2 + " is the winner 🥳" ;
 }
 else {
     document.querySelector("h1").innerHTML = "DRAW" ;
